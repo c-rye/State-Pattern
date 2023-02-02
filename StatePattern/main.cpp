196a0ba98bc205bafe4e76e95cf8bf44cf029c9f
c-rye <caleb@calebrye.com>
2023-02-02 17:26:35 -0600

    Comments and cout statements added for clarity.

--- StatePattern/main.cpp
+++ StatePattern/main.cpp
@@ -1,8 +1,15 @@
 # include "state.h"
 
 int main() {
+
+	// create an engine object that is in the given state
 	Engine engine1(new ConcreteStateB);
-	engine1.requestA();
+
+	// request that our engine perform function A and transition to state A
 	engine1.requestA();
 
+	// at this point, the engine should already be in state A, but will still execute function A
+	engine1.requestA();
+
+	
 }
--- StatePattern/state.h
+++ StatePattern/state.h
@@ -27,23 +27,37 @@ private:
 public:
 	Engine(State* state) : _state(nullptr) {
 		transitionTo(state);
+		std::cout << "Engine constructed of type: " << typeid(this).name() << "\n";
 	}
 
 	~Engine() { delete _state; }
 
+	// main function for transitioning between states
 	void transitionTo(State* state) {
+		std::cout << "transitionTo function called...\n";
+		// if a state already exists for this engine, delete it
+		
 		if (_state) delete _state;
 
+		// replace the newly deleted _state variable with the new state
 		_state = state;
+
+		// set the engine to the new state
+		std::cout << "Calling set_engine...\n";
 		_state->set_engine(this);
+		std::cout << "set_engine complete.\n";
+		std::cout << "Transition successful!\n";
 	}
 
 	void requestA() { _state->doActionA(); }
 	void requestB() { _state->doActionB(); }
 };
 
+// define our first "actual" state we can be in
 class ConcreteStateA : public State {
 public:
+
+	// define two functions that our state can perform
 	void doActionA() override {
 		
 		std::cout << typeid(this).name() << " called doActionA()\n";
@@ -53,14 +67,18 @@ public:
 	void doActionB() override {
 	
 		std::cout << typeid(this).name() << " called doActionB()\n";
-		
+		//_engine->transitionTo(new ConcreteStateB);
 	}
 };
 
+// define our second "actual" state we can be in
 class ConcreteStateB : public State {
 public:
+
+	//define two functions our state can perform
 	void doActionA() override {
 
+		// this action will transition us from state B to A
 		std::cout << typeid(this).name() << " called doActionA()\n";
 		_engine->transitionTo(new ConcreteStateA);
 	}
@@ -68,6 +86,6 @@ public:
 	void doActionB() override {
 
 		std::cout << typeid(this).name() << " called doActionB()\n";
-
+		_engine->transitionTo(new ConcreteStateB);
 	}
 };
